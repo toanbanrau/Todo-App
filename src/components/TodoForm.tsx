27b567e17c0cf44cc652e2bcd1ec132ca02a9e5c
toanbanrau <thieutoan2004@gmail.com>
2025-07-07 17:20:18 +0700

    fix: css form

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -82,9 +82,18 @@ const TodoForm = ({ statusAdd, todo, onClose }: todoFormProps) => {
     <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
       <form
         onSubmit={handleSubmit(onsubmit)}
-        className="grid grid-cols-1 gap-5 overflow-y-auto md:flex max-w-[33rem] max-h-[80vh] min-w-[20rem] border-2 border-gray-300 p-5 rounded-2xl bg-white"
+        className="flex flex-col max-h-[90vh] min-w-[20rem] border-2 border-gray-300 p-5 rounded-2xl bg-white"
       >
-        <div className="flex flex-col">
+        <div className="flex justify-between">
+          <h1 className="text-2xl font-bold">
+            {todo ? "Update Todo" : "Add Todo"}
+          </h1>
+          <button className=" flex content-center items-center hover:bg-gray-300 cursor-pointer">
+            {" "}
+            <IconCancel onClick={() => onClose()} />
+          </button>
+        </div>
+        <div className="flex flex-col gap-3 overflow-y-auto max-h-full p-2">
           <div className="flex flex-col gap-1">
             <label htmlFor="todo-title" className="font-medium text-gray-700">
               Title
@@ -100,7 +109,54 @@ const TodoForm = ({ statusAdd, todo, onClose }: todoFormProps) => {
               <p className="text-red-500 text-sm">{errors.title.message}</p>
             )}
           </div>
-          <div className="flex flex-col gap-1">
+
+          <div className="flex flex-col">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="priority" className="font-medium text-gray-700">
+                Tags
+              </label>
+              <Controller
+                name="tags"
+                control={control}
+                render={({ field }) => {
+                  const { value, onChange } = field;
+                  const tags = value.map((text: string) => ({
+                    id: "",
+                    text,
+                    className: "my-tag",
+                  }));
+                  const handleDelete = (index: number) => {
+                    onChange(value.filter((_, i) => i !== index));
+                  };
+                  const handleAddition = (tag: Tag) => {
+                    if (tag.text.trim().length > 8) return;
+                    if (
+                      value.some(
+                        (t: string) =>
+                          t.trim().toLowerCase() ===
+                          tag.text.trim().toLowerCase(),
+                      )
+                    ) {
+                      return;
+                    }
+                    onChange([...value, tag.text]);
+                  };
+                  return (
+                    <ReactTags
+                      tags={tags}
+                      handleDelete={handleDelete}
+                      inputFieldPosition="top"
+                      handleAddition={handleAddition}
+                    />
+                  );
+                }}
+              />
+            </div>
+            {errors.tags && (
+              <p className="text-red-500 text-sm">{errors.tags.message}</p>
+            )}
+          </div>
+          <div className="flex flex-col">
             <label htmlFor="todo-desc" className="font-medium text-gray-700">
               Description
             </label>
@@ -112,137 +168,95 @@ const TodoForm = ({ statusAdd, todo, onClose }: todoFormProps) => {
               id=""
             ></textarea>
           </div>
-          <div className="flex flex-col gap-1">
-            <label htmlFor="priority" className="font-medium text-gray-700">
-              Tags
-            </label>
-            <Controller
-              name="tags"
-              control={control}
-              render={({ field }) => {
-                const { value, onChange } = field;
-                const tags = value.map((text: string) => ({
-                  id: "",
-                  text,
-                  className: "my-tag",
-                }));
-                const handleDelete = (index: number) => {
-                  onChange(value.filter((_, i) => i !== index));
-                };
-                const handleAddition = (tag: Tag) => {
-                  if (tag.text.trim().length > 8) return;
-                  if (
-                    value.some(
-                      (t: string) =>
-                        t.trim().toLowerCase() ===
-                        tag.text.trim().toLowerCase(),
-                    )
-                  ) {
-                    return;
-                  }
-                  onChange([...value, tag.text]);
-                };
-                return (
-                  <ReactTags
-                    tags={tags}
-                    handleDelete={handleDelete}
-                    inputFieldPosition="top"
-                    handleAddition={handleAddition}
-                  />
-                );
-              }}
-            />
-          </div>
-          {errors.tags && (
-            <p className="text-red-500 text-sm">{errors.tags.message}</p>
-          )}
-        </div>
-
-        <div className="fex flex-col">
-          <div className="flex flex-col gap-1">
-            <label htmlFor="priority" className="font-medium text-gray-700">
-              Priority
-            </label>
-            <select
-              id="priority"
-              {...register("priority")}
-              className="px-3 py-2 border border-slate-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
-            >
-              <option value="low">Low</option>
-              <option value="medium">Medium</option>
-              <option value="high">High</option>
-            </select>
-          </div>
-          <div className="flex flex-col gap-1">
-            <label htmlFor="priority" className="font-medium text-gray-700">
-              Status
-            </label>
-            <select
-              id="priority"
-              {...register("status")}
-              className="px-3 py-2 border border-slate-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
-            >
-              {Object.values(todoStatus).map((statusAdd) => (
-                <option key={statusAdd} value={statusAdd}>
-                  {statusAdd}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="flex flex-col gap-1">
-            <label
-              htmlFor="todo-deadline"
-              className="font-medium text-gray-700"
-            >
-              Deadline
-            </label>
-            <input
-              id="todo-deadline"
-              className="px-3 py-2 border border-slate-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
-              type="datetime-local"
-              lang="en-GB"
-              {...register("deadline")}
-            />
-            {errors.deadline && (
-              <p className="text-red-500 text-sm">{errors.deadline.message}</p>
-            )}
-          </div>
-          <div className="flex flex-col gap-1">
-            <label htmlFor="todo-title" className="font-medium text-gray-700">
-              Thumbnail
-            </label>
-            <input
-              id="todo-title"
-              className="px-3 py-2 border border-slate-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
-              type="text"
-              {...register("thumbnail")}
-              placeholder="Add Todo"
-            />
-            {errors.thumbnail && (
-              <p className="text-red-500 text-sm">{errors.thumbnail.message}</p>
-            )}
-          </div>
-          <div className=" py-2">
-            {watch("thumbnail") && (
-              <img
-                src={watch("thumbnail")}
-                alt="Thumbnail"
-                className="w-full h-40 object-cover rounded border"
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-3">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="priority" className="font-medium text-gray-700">
+                Priority
+              </label>
+              <select
+                id="priority"
+                {...register("priority")}
+                className="px-3 py-2 border border-slate-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+              >
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+              </select>
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="priority" className="font-medium text-gray-700">
+                Status
+              </label>
+              <select
+                id="priority"
+                {...register("status")}
+                className="px-3 py-2 border border-slate-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+              >
+                {Object.values(todoStatus).map((statusAdd) => (
+                  <option key={statusAdd} value={statusAdd}>
+                    {statusAdd}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex flex-col gap-1">
+              <label
+                htmlFor="todo-deadline"
+                className="font-medium text-gray-700"
+              >
+                Deadline
+              </label>
+              <input
+                id="todo-deadline"
+                className="px-3 py-2 border border-slate-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                type="datetime-local"
+                lang="en-GB"
+                {...register("deadline")}
               />
-            )}
+              {errors.deadline && (
+                <p className="text-red-500 text-sm">
+                  {errors.deadline.message}
+                </p>
+              )}
+            </div>
           </div>
-          <div className="flex items-center w-full gap-1">
-            <button
-              className="cursor-pointer w-full px-8 py-2 bg-gradient-to-b from-violet-500 to-pink-500 text-white rounded font-semibold"
-              type="submit"
-            >
-              {todo ? "Update" : "Add"}
-            </button>
-            <button className=" flex content-center items-center hover:bg-gray-300 cursor-pointer">
-              {" "}
-              <IconCancel onClick={() => onClose()} />
-            </button>
+
+          <div className="fex flex-col">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="todo-title" className="font-medium text-gray-700">
+                Thumbnail
+              </label>
+              <input
+                id="todo-title"
+                className="px-3 py-2 border border-slate-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                type="text"
+                {...register("thumbnail")}
+                placeholder="Add Todo"
+              />
+              {errors.thumbnail && (
+                <p className="text-red-500 text-sm">
+                  {errors.thumbnail.message}
+                </p>
+              )}
+            </div>
+            <div className=" py-2">
+              {watch("thumbnail") && (
+                <img
+                  src={watch("thumbnail")}
+                  alt="Thumbnail"
+                  className="h-30 object-cover rounded border"
+                />
+              )}
+            </div>
           </div>
+        </div>
+        <div className="flex items-between gap-1">
+          <button
+            className="cursor-pointer w-full px-8 py-2 bg-gradient-to-b from-violet-500 to-pink-500 text-white rounded font-semibold"
+            type="submit"
+          >
+            {todo ? "Update" : "Add"}
+          </button>
         </div>
       </form>
     </div>
